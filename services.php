d0f442e5540e9b511a40d0400680211bbd9580f3
growviralinsta-spec <growviralinsta@gmail.com>
2026-08-20 06:50:41 +0530

    Delete services.php

--- services.php
+++
@@ -1,8 +0,0 @@
-<?php
-header('Content-Type: application/json; charset=utf-8');
-$file=__DIR__.'/data.json';
-$data=file_exists($file)?json_decode(file_get_contents($file),true):['services'=>[]];
-$out=[];
-foreach(($data['services']??[]) as $s){if(!empty($s['active'])){$out[]=$s;}}
-echo json_encode($out);
-?>
